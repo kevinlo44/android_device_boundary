610f86e80b48ce5fded330b6e94231942a7de808
Gary Bisson <gary.bisson@boundarydevices.com>
2018-05-03 17:23:58 +0200

    bootscript.mk: add i.MX8 support (arm64)
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- bootscript.mk
+++ bootscript.mk
@@ -1,3 +1,4 @@
+ifeq ($(BOARD_SOC_CLASS),IMX6)
 BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/boot.scr
 $(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_PRODUCT)/bootscript.txt
 	mkdir -p $(dir $@)
@@ -7,6 +8,12 @@ UPGRADE_TARGET := $(PRODUCT_OUT)/boot/upgrade.scr
 $(UPGRADE_TARGET): $(UBOOT_IMX_PATH)/uboot-imx/board/boundary/bootscripts/upgrade.txt
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "upgrade script" -d $< $@
+else
+BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/boot.scr
+$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_PRODUCT)/bootscript.txt
+	mkdir -p $(dir $@)
+	mkimage -A arm64 -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
+endif
 
 .PHONY: bootscript
 bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET)
