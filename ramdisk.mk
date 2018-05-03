3515aa719b483e1a4aa41a2a754408334c3e0dc5
Gary Bisson <gary.bisson@boundarydevices.com>
2018-05-03 17:24:16 +0200

    ramdisk.mk: add i.MX8 support (arm64)
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- ramdisk.mk
+++ ramdisk.mk
@@ -1,6 +1,13 @@
+ifeq ($(BOARD_SOC_CLASS),IMX6)
 RAMDISK_TARGET := $(PRODUCT_OUT)/boot/uramdisk.img
 $(RAMDISK_TARGET): $(PRODUCT_OUT)/ramdisk.img kernelmodules
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T ramdisk -n "RAM Disk" -d $< $@
+else
+RAMDISK_TARGET := $(PRODUCT_OUT)/boot/uramdisk.img
+$(RAMDISK_TARGET): $(PRODUCT_OUT)/ramdisk.img kernelmodules
+	mkdir -p $(dir $@)
+	mkimage -A arm64 -O linux -T ramdisk -n "RAM Disk" -d $< $@
+endif
 
 bootimage: $(RAMDISK_TARGET)
